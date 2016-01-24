d4b8d91ceadd7243665ff2471b26706b99caae8a
Ignatev Andrej <joomlacoder16@yandex.ru>
2016-01-24 20:40:29 +0300

    Do do

---
+++ src/xyz/ignatyev/Graphics/SpriteSheet.java
@@ -0,0 +1,25 @@
+package xyz.ignatyev.Graphics;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by Andrej on 24.01.2016.
+ */
+public class SpriteSheet {
+    private BufferedImage       sheet;
+    private int                 spriteCount;
+    private int                 scale;
+    private int                 spritesInWidth;
+
+    public SpriteSheet(BufferedImage sheet, int spriteCount, int scale){
+        this.sheet = sheet;
+        this.spriteCount = spriteCount;
+        this.scale = scale;
+        spritesInWidth = sheet.getWidth() / scale;
+    }
+
+    /*public BufferedImage getSprite(int index){
+        index = index % spriteCount;
+        return
+    }*/
+}
